6fac4546b30838ce3c9015a3e153a2cbb465f2f2
nkilli2022 <119540269+nkilli2022@users.noreply.github.com>
2023-02-02 13:20:45 -0700

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,3 @@
 provider "azurerm" {
   features {}
-
-  subscription_id = "0b872b07-cd78-4c77-9d82-da2c583b9db9"
-  client_id   = "da50a1fb-8530-45aa-8417-44473313f2be"
-   client_secret   = "IKZ8Q~J~ZLC9BfDPcMWNGDJMlRiFyC9y2TiOUbC3"
-  tenant_id       = "ed1b8923-33bb-4637-a1fd-30df2c27bd76"
   }
